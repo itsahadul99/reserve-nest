33c128add91eea6997b60a27d4c8bb36a4cf6620
itsahadul99 <itsahadul999@gmail.com>
2024-05-09 20:07:39 +0600

    basic setup done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-6xl font-poppins font-bold">I am from home</h1>
+        </div>
+    );
+};
+
+export default Home;
