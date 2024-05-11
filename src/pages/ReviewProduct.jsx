cb1fa59054f814a7a0dd741990579176d2bc3433
itsahadul99 <itsahadul999@gmail.com>
2024-05-11 16:12:11 +0600

    added review and store review data on database

--- src/pages/ReviewProduct.jsx
+++ src/pages/ReviewProduct.jsx
@@ -1,17 +1,36 @@
 /* eslint-disable no-unused-vars */
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const ReviewProduct = () => {
-    const {user} = useAuth()
-    const {id} = useParams();
+    const { user } = useAuth()
     const data = useLoaderData();
-    console.log(data);
+    const navigate = useNavigate()
+    const handleReview = e => {
+        e.preventDefault()
+        const form = e.target;
+        const comment = form.comment.value;
+        const rating = form.rating.value;
+        const reviewData = { comment, rating, user_name: user?.displayName, room_title: data?.room_title, user_profile_img: user?.photoURL }
+        axios.post(`${import.meta.env.VITE_API_URL}/reviews`, reviewData)
+        .then(res => {
+            if(res.data.insertedId){
+                Swal.fire({
+                    title: "Success!",
+                    text: "Review successfully done",
+                    icon: "success"
+                  });
+                  navigate('/my-bookings')
+            }
+        })
+    }
     return (
         <div className="max-w-7xl mx-auto my-5 md:my-10">
             <div>
-            <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-center">Write your review here</h1>
-                <form className="w-full md:w-1/2 mx-auto my-5 md:my-10 shadow-md p-5 bg-gray-50">
+                <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-center">Write your review here</h1>
+                <form onSubmit={handleReview} className="w-full md:w-1/2 mx-auto my-5 md:my-10 shadow-md p-5 bg-gray-50">
                     <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
                         <div>
                             <label className='text-gray-700 font-semibold' htmlFor='userName'>
@@ -53,7 +72,7 @@ const ReviewProduct = () => {
                     </div>
                     <div className='flex justify-end mt-6'>
                         <button className='px-2 md:px-6 py-1 md:py-2 hover:bg-[#91D9D0] bg-[#5beeddd4] duration-300 rounded-md text-sm md:text-lg font-medium text-white'>
-                            Save
+                            Save Review
                         </button>
                     </div>
                 </form>
